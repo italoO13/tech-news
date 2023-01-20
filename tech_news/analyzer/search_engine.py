813e946ff54994ac52356642dea9d5008f6093c3
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-01-20 14:28:06 -0300

    feat: função search_by_date que filtra os dados por data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,6 @@
 from tech_news.database import search_news
+from datetime import datetime
+
 
 # Requisito 6
 
@@ -10,7 +12,12 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date_formated = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        news = search_news({"timestamp": {"$eq": date_formated}})
+        return [(new["title"], new["url"]) for new in news]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
